99ed19183aa30e15cdc2a5788381eb70bacacaf2
Patrick Reis <patrickbernardor@gmail.com>
2023-08-08 23:28:56 -0300

    feat: speaker integrate

--- scripts/chip8.js
+++ scripts/chip8.js
@@ -1,6 +1,9 @@
 import Renderer from './renderer.js';
+import Keyboard from './keyboard.js';
 
 const renderer = new Renderer(10);
+const Keyboard = new Keyboard();
+
 let loop;
 
 let fps = 60, fpsInterval, startTime, now, then, elapsed;
